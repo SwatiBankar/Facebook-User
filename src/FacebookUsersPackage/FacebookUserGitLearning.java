a1d9aa8644d623ac5283e292f64c540de32a513a
SwatiBankar <swatibankar88@gmail.com>
2019-07-15 12:15:16 +0530

    my first commit to do practice

---
+++ src/FacebookUsersPackage/FacebookUserGitLearning.java
@@ -0,0 +1,12 @@
+package FacebookUsersPackage;
+
+public class FacebookUserGitLearning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubystem
+		System.out.println("I am Learning git");
+		System.out.println("When their is will there is well ");
+
+	}
+
+}
